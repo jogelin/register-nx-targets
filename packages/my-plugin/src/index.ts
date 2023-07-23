7580b5e00e5111668f816b47a4d7eae64b4eb9d1
jgelin <jgelin@seligent.com>
2023-07-23 15:24:27 +0200

    Register build target

--- packages/my-plugin/src/index.ts
+++ packages/my-plugin/src/index.ts
@@ -1,9 +1,28 @@
 import {TargetConfiguration} from "nx/src/config/workspace-json-project-json";
+import {normalizePath} from 'nx/src/utils/path';
+
+export const projectFilePatterns = ['project.json'];
 
 export function registerProjectTargets(projectPath: string): Record<string, TargetConfiguration> {
+  const normalizedPath = normalizePath(projectPath);
+  const projectName = normalizedPath.substring(0, normalizedPath.lastIndexOf('/'));
+
   return {
     build: {
-
+      "executor": "@nx/vite:build",
+      "outputs": ["{options.outputPath}"],
+      "defaultConfiguration": "production",
+      "options": {
+        "outputPath": `dist/packages/${projectName}`
+      },
+      "configurations": {
+        "development": {
+          "mode": "development"
+        },
+        "production": {
+          "mode": "production"
+        }
+      }
     },
   };
 }
